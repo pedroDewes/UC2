f627f2cd9e3d448f6664320c4e53ed05b4abc823
pedroDewes <pedrolimadewes@gmail.com>
2025-09-29 21:56:32 -0300

    Adicionado na pasta aulas

---
+++ Aula6/Aula7/desafio.js
@@ -0,0 +1,28 @@
+/*const primeiroNumero = 10
+const segundoNumero = 20
+
+
+const resultado = primeiroNumero - segundoNumero
+
+console.log(resultado)*/
+
+const primeiroValor= 345
+const SegundoValor= 10
+const novoResultado = primeiroValor / SegundoValor
+console.log(novoResultado)
+
+let minhaIdade = 33
+minhaIdade = minhaIdade + 1
+console.log(minhaIdade)
+
+let resultadoSimples = 100
+let resultadoSimples += 50
+console.log(resultadoSimples) 
+
+resultadoSimples += 100
+console.log(resultadoSimples)
+resultadoSimples *=2
+console=(resultadoSimples)
+
+
+
---
+++ Aulas/Aula4/Exemplo1/aula5/atividade.js
@@ -0,0 +1,45 @@
+let roupaAzul = prompt("Voce esta vestindo roupa azul?")
+let bermudaRosa = prompt("Voce esta vestindo uma bermuda rosa?")
+let oculosEscuro = prompt("Voce esa vestindo um oculos escuro?")
+
+
+
+console.log("Voce esta vestindo roupa azul?", roupaAzul)
+console.log("Voce esta vestindo uma bermuda rosa?",bermudaRosa )
+console.log("Voce esa vestindo um oculos escuro?", oculosEscuro)
+
+//Exercício 3 
+
+let a = 10
+let b = 25
+let c = b
+
+// Aqui fazermos uma lógica para trocar os valores
+
+c = b
+
+b = a
+
+a = c
+
+// Depois de trocados, teremos o seguinte resultado
+
+console.log("O novo valor de a é", a)
+console.log("O novo valor de b é", b)
+
+// Desafio proposto pelo professor
+
+
+
+const e = prompt("escreve um número")
+
+const f = prompt("Escolha outro outro número")
+
+ const x = e + f
+ const y = e * f
+
+console.log(x)
+console.log(y)
+
+e = Number
+f = Number
