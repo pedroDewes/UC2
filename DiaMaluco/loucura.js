1ef2204240713d1c64a7f1100fd668e27bddd6e9
pedroDewes <pedrolimadewes@gmail.com>
2025-10-10 21:53:53 -0300

    Adicionado na pasta Aulas

---
+++ DiaMaluco/loucura.js
@@ -0,0 +1,47 @@
+ /*const valor1 = Number(prompt("De uma valor de 1 a 10, sobre o número de acessório"))
+
+const valor2 = Number(prompt("De uma valor de 1 a 10, sobre número de cores."))
+
+const valor3 = Number(prompt("De uma valor de 1 a 10, sobre fator brilho."))
+
+let resultado = (valor1 * 5)+( valor2 * 3)+(valor3 * 10)
+console.log(resultado, "Foi sua pontuação.")
+
+let metaPontuacao = 80
+
+let aprovado = resultado >= metaPontuacao
+
+metaPontuacao = Number(metaPontuacao)
+
+ const resulatado2 = metaPontuacao - resultado
+
+if(aprovado === true) {console.log("Aprovado")}
+
+else console.log(resulatado2, "pontos, foi o que faltou para ser aprovado!") */
+
+let descricao = prompt("Descreva seu look!")
+let corPrincipal = prompt("Qual é a cor principal do seu look?")
+
+console.log(descricao.length)
+
+let tamanhoAdequado = descricao.length >= 20
+
+console.log("O look tem mais de 20 letras?", tamanhoAdequado)
+
+descricao = descricao.toLowerCase()
+
+let palavrasChaves = ["Petro", "Azul", "Vemelho", "Rosa", "Cinza", "Roxo", "Verde", "Amarelo"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
